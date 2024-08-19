fcc91d747b2f23e028a8e3d396cf774c8e68812a
UalissonLima <ualisson.robertolima@gmail.com>
2024-08-18 20:10:58 -0400

    alterando texto do h1 do home

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -61,7 +61,7 @@ export function Home() {
       <Header />
       <div className="flex flex-col w-full py-4 items-center justify-center">
         <h1 className="md:text-4xl text-3xl font-bold text-white mt-20">
-          Ualisson Lima
+          Agora tu acredita que é meu danada?
         </h1>
 
         <span className="text-gray-50 mb-5 mt-3">Veja meus links👇</span>
